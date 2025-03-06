65e100f50850cdffd3cee225ad5405ac74bdfcd4
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-03-06 16:05:18 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 37104 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,4 +1,5 @@
-aList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from string import ascii_lowercase
+aList = list(ascii_lowercase)
 
 L = int(input())
 a_list = input()
@@ -6,4 +7,4 @@ ans = 0
 
 for i in range(L):
     ans += (aList.index(a_list[i]) + 1) * (31 ** i)
-print(ans)
+print(ans % 1234567891)
