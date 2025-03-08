3702e4f1b28c58417f2df62bc8638c6f669047fb
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-03-08 23:11:30 +0900

    [Silver V] Title: 나이순 정렬, Time: 216 ms, Memory: 58068 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,10 +1,10 @@
 import sys
 readline = sys.stdin.readline
-output = sys.stdout
+output = sys.stdout.write
 
 N = int(readline().strip())
 persons = [list(map(str, readline().split())) for i in range(N)]
 p_new = sorted(persons, key= lambda x: int(x[0]))
 
 for person in p_new:
-    print(" ".join(person))
+    output(" ".join(person) + "\n")
