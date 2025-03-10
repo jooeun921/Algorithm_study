5d9d74d49b9fa105d955175f52c620b8ab40ef4d
jooeun921 <jooeun921@gmail.com>
2025-03-10 21:25:59 +0900

    [Silver IV] Title: 요세푸스 문제 0 - 다른 풀이 및 메모

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -8,3 +8,17 @@ while len(N_list) != 0:
     ans.append(N_list.pop(state))
     
 print("<" + ", ".join(map(str, ans)) + ">")
+
+
+# 다른 풀이
+# from collections import deque
+
+# N, K = map(int, input().split())
+# queue = deque(range(1, N + 1))
+# ans = []
+
+# while queue:
+#     queue.rotate(-(K - 1))  # K-1번 왼쪽으로 회전 (K번째 사람이 맨 앞에 오도록)
+#     ans.append(queue.popleft())  # K번째 사람을 제거하여 정답 리스트에 추가
+
+# print("<" + ", ".join(map(str, ans)) + ">")
