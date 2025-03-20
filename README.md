f504823385e3dbf295361e9d7be042fd40e99386
jooeun921 <jooeun921@gmail.com>
2025-03-20 23:28:59 +0900

    03/20 문제 풀이 일부

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Algorithm_study
+
 This is an auto push repository for Baekjoon Online Judge created with [BaekjoonHub](https://github.com/BaekjoonHub/BaekjoonHub).
+
+개인적으로 느끼는 체감 난이도는 문제 이름 옆에 '' 안에 아래 이모티콘으로 표시하고자 한다.
+⭐ : 난이도. 개수가 늘수록 어려웠다는 것! 우선 최대는 별 다섯개를 두고, 크게 고민하지 않고 풀었다면 하나.
+❣️ : (좀 더 쉽게 풀 수 있었던 문제) 새로운 개념을 필요로 하거나 생각해볼만한 문제에 준다. 난이도 자체는 쉽게 느꼈어도 더 간단하게 풀 수 있었던 경우에도 준다.
--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -6,3 +6,17 @@ for i in range(n):
     if li[i] == v:
         ans += 1
 print(ans)
+
+
+# 처음 풀이인데, 사실 위에를 먼저 생각했다가 값이 없을 때 문제가 생기지 않나? 싶었음. 그래서 만든 방식인데,
+# 위로 한번 해보니까 되더라~
+# import sys
+# board = [list(sys.stdin.readline().strip()) for _ in range(5)]
+# ans = ''
+# length = len(board)
+# while board != [[], [], [], [], []]:
+#     for i in range(5):
+#         if len(board[i]) != 0:
+#             ans += board[i].pop(0)
+
+# print(ans)
