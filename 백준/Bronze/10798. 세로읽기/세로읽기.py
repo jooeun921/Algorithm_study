efb40283ece43151e697f1c103c7ec6c0b3db0dc
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-03-20 23:07:58 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,10 +1,6 @@
-import sys
-board = [list(sys.stdin.readline().strip()) for _ in range(5)]
-ans = ''
-length = len(board)
-while board != [[], [], [], [], []]:
-    for i in range(5):
-        if len(board[i]) != 0:
-            ans += board[i].pop(0)
+n = [input() for i in range(5)]
 
-print(ans)
+for i in range(15):
+    for j in range(5):
+        if i < len(n[j]):
+            print(n[j][i],end='')
