b2c42bed1a44f25e6a3800b2a404f07e2e160d81
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-03-22 18:40:13 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,17 +1,11 @@
-def 재귀_횟수(n):
-    fibo = {0:0, 1: 1}
-    if n == 0:
-        zero = 1
-        one = 0
-    else:
-        for i in range(2, n+1):
-            fibo[i] = fibo[i-1] + fibo[i-2]
-        zero = fibo[n-1]
-        one = fibo[n]
-    return zero, one
-
 T = int(input())
 for _ in range(T):
+    a, b = 0, 1
     n = int(input())
-    zero, one = 재귀_횟수(n)
-    print(zero, one)
+    if n == 0 :
+        a = 1
+        b = 0
+    else:
+        for _ in range(n-1):
+            a, b = b, a + b
+    print(a, b)
