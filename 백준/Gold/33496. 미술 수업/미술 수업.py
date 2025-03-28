ddf598ac2ca04443e5b028f93b1b00cc141f8b6f
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-03-28 22:28:59 +0900

    [Gold IV] Title: 미술 수업, Time: 796 ms, Memory: 141024 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/33496. 미술 수업/미술 수업.py
+++ 백준/Gold/33496. 미술 수업/미술 수업.py
@@ -5,15 +5,24 @@ N_list = [list(map(int, stdin.readline().split())) for _ in range(N)]
 
 diag_plus = set(x - y for x, y in N_list)
 diag_minus = set(x + y for x, y in N_list)
-meet = diag_plus & diag_minus
-example = diag_plus | diag_minus
-example -= meet
 
+# meet은 교집합, example은 합집합에서 교집합을 뺀 부분
+meet = diag_plus & diag_minus
+example = (diag_plus | diag_minus) - meet
+
+# diag_plus와 diag_minus를 정렬
+diag_plus = sorted(diag_plus)
+diag_minus = sorted(diag_minus)
+
+# cnt 계산
 cnt = 0
-diag_plus = list(diag_plus)
-diag_minus = list(diag_minus)
-for i in range(len(diag_plus)):
-    for j in range(len(diag_minus)):
-        if diag_plus[i] < diag_minus[j]:
-            cnt += 1
+i, j = 0, 0
+while i < len(diag_plus) and j < len(diag_minus):
+    if diag_plus[i] < diag_minus[j]:
+        cnt += len(diag_minus) - j  # diag_plus[i]보다 큰 diag_minus의 개수
+        i += 1
+    else:
+        j += 1
+
+# 최종 출력
 print(cnt + len(meet) + len(example))
