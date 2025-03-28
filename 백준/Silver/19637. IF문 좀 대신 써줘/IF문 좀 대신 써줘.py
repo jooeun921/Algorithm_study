c8d0acbc483048329a9c0c32c2843c7620d99aea
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-03-28 20:14:41 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 248 ms, Memory: 64476 KB -BaekjoonHub

---
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -0,0 +1,12 @@
+from sys import stdin
+from bisect import bisect_left
+
+n, m = map(int, stdin.readline().split())
+n_list = [list(stdin.readline().strip().split()) for _ in range(n)]
+m_list = [int(stdin.readline()) for _ in range(m)]
+
+n_num_list = list(int(n_list[i][1]) for i in range(n))
+
+for i in range(m):
+    idx = bisect_left(n_num_list, m_list[i])
+    print(n_list[idx][0])
