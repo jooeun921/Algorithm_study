694db6ba34a1b4cf9b2170dacf0cbb8f91c48bfd
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-03-30 17:09:13 +0900

    [Bronze IV] Title: 상근날드, Time: 36 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Bronze/5543. 상근날드/상근날드.py
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -1,12 +1,5 @@
 from sys import stdin
 
-상덕버거 = int(stdin.readline())
-중덕버거 = int(stdin.readline())
-하덕버거 = int(stdin.readline())
-콜라 = int(stdin.readline())
-사이다 = int(stdin.readline())
-
-햄부르스딱스 = min(상덕버거, 중덕버거, 하덕버거)
-음료 = min(콜라, 사이다)
-
-print(햄부르스딱스 + 음료 - 50)
+햄부기 = min(int(stdin.readline()) for _ in range(3))
+음료 = min(int(stdin.readline()) for _ in range(2))
+print(햄부기 + 음료 - 50)
