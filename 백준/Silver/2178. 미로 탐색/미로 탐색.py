8a47cacb752a043e8fd11462cc922192ba7d4680
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-04-22 23:57:32 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34936 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,29 @@
+from sys import stdin
+from collections import deque
+
+N, M = map(int, stdin.readline().split())
+maze = [list(map(int, stdin.readline().strip())) for _ in range(N)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque([(x, y)])
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                continue
+
+            if maze[nx][ny] == 0:
+                continue
+
+            if maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1
+                queue.append((nx, ny))
+
+    return maze[N-1][M-1]
+
+print(bfs(0, 0))
