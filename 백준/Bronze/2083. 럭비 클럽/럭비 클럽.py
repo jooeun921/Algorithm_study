702af24b844ce31540c0ee34cca503c30a361e5d
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-05-15 09:06:23 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,10 @@
+from sys import stdin
+
+while True:
+    name, age, weight = map(str, stdin.readline().split())
+    if name == '#' and age == '0' and weight == '0':
+        break
+    if int(age) > 17 or int(weight) >= 80:
+        print(name, "Senior")
+    else:
+        print(name, "Junior")
