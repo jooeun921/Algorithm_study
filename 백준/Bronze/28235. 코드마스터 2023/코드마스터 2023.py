72a420c77bdf385596a35b4a778cee4691e015ac
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-05-20 19:00:15 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+type_comp = input()
+
+if type_comp == 'SONGDO':
+    print("HIGHSCHOOL")
+elif type_comp == 'CODE':
+    print("MASTER")
+elif type_comp == '2023':
+    print("0611")
+elif type_comp == 'ALGORITHM':
+    print("CONTEST")
