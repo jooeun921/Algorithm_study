7fb3a4a1ef1e2f934d45f7ac620184d238dd8039
jooeun <86337975+jooeun921@users.noreply.github.com>
2025-10-10 22:47:18 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,12 +1,2 @@
 def solution(s):
-    ans = []
-    str_list = list(s)
-    for i in str_list:
-        if str_list.count(i) == 1:
-            ans.append(i)
-    if len(ans) >= 1:
-        ans.sort()
-        answer = ''.join(ans)
-    else:
-        answer = ''
-    return answer
+    return "".join(sorted([ cha for cha in set(s) if s.count(cha) == 1]))
